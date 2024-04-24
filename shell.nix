07031aac993b6759a5a0802554626842d5097a1e
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2024-04-24 13:21:26 -0400

    added validation

--- shell.nix
+++ shell.nix
@@ -19,38 +19,11 @@ mkShell rec{
     vulkan-validation-layers
   ];
 
-  # nix-ld.enable = true;
-  # nix-ld.libraries = with pkgs; [
-  # ];
-
   # If it doesn’t get picked up through nix magic
-  VULKAN_SDK = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+  # VULKAN_SDK = "${vulkan-validation-layers}/share/vulkan/explicit_layer.d";
   shellHook = ''
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}"
+    export VULKAN_SDK="${vulkan-headers}"
+    export VK_LAYER_PATH="${vulkan-validation-layers}/share/vulkan/explicit_layer.d"
   '';
 }
-
-# pkgs.mkShell rec {
-
-#   buildInputs = with pkgs; [
-#     rustup
-
-#     xorg.libX11
-#     xorg.libXcursor
-#     xorg.libXrandr
-#     xorg.libXi
-
-#     shaderc
-#     directx-shader-compiler
-#     libGL
-#     vulkan-headers
-#     vulkan-loader
-#     vulkan-tools
-#     vulkan-tools-lunarg
-#     vulkan-validation-layers
-#   ];
-
-#   shellHook = ''
-#     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
-#   '';
-# }
