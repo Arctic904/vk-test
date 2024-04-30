c59d3548d20b886da286754c4645d05c24bd0015
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2024-04-30 19:44:07 -0400

    reset swapchain

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/home/ryan/.nix-profile/bin/fish
 
 trap 'kill 0' SIGINT;
-cargo watch -w src/ -x run & 
+cargo watch -w src/ -w compiled/ -x run & 
 cargo watch -w shaders/ -s "fish ./compile_shaders.fish" --use-shell=bash
