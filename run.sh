fecca15e726227bf2c8be231e9cf233620899126
Arctic904 <=>
2024-05-14 01:30:10 -0400

    modify run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/home/ryan/.nix-profile/bin/fish
+#!/sbin/fish
 
 trap 'kill 0' SIGINT;
-cargo watch -w src/ -w compiled/ -x run & 
-cargo watch -w shaders/ -s "fish ./compile_shaders.fish" --use-shell=bash
+cargo watch -w shaders/ -s "fish ./compile_shaders.fish" --use-shell=bash &
+cargo watch -w src/ -w compiled/ -x run 
